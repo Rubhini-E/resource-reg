6dbc20efa2a55529781ab3310aed8b49947bc2f9
Rubhini-E <121244864+Rubhini-E@users.noreply.github.com>
2023-01-06 16:32:39 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace reg_resource
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          if((txtname.Text==" ")&&(txtemail.Text==" ")&&(txtadd.Text==" "))
+                {
+                MessageBox.Show("REGISTERED SUCCESSFULLY","REGISTER" ,MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+          else
+            {
+                MessageBox.Show("ERROR!!!!!ENTER ALL DETAILS","ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+    }
+}
